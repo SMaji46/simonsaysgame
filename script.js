a5c24c7bde2501ca602d541a1879236d5ab970be
SMaji46 <subhajitmaji2003@gmail.com>
2024-09-21 15:45:37 +0530

    upgraded

--- script.js
+++ script.js
@@ -1,63 +1,83 @@
-let gameseq=[];
-let userseq=[];
-let start=false;
-let level=-1;
-let max=level;
-let btns=["red","yellow","purple","green"];
-document.addEventListener("keyup",function(){
-    if(start==false)
-    {
-        levelUp();
-        start=true;
+// Initialize variables
+var buttonColors = ["red", "blue", "green", "yellow"];
+var gamePattern = [];
+var userClickedPattern = [];
+var started = false;
+var level = -1;
+let max=0;
+// Start the game when any key is pressed
+$(document).keydown(function() {
+  if (!started) {
+    nextSequence();
+    started = true;
+  }
+});
+
+// When a button is clicked
+$(".button").click(function() {
+  var userChosenColor = $(this).attr("id");
+  userClickedPattern.push(userChosenColor);
+
+  playSound(userChosenColor);
+  animatePress(userChosenColor);
+
+  checkAnswer(userClickedPattern.length - 1);
+});
+
+// Function to generate the next sequence
+function nextSequence() {
+  userClickedPattern = [];
+  level++;
+  max=Math.max(max,level);
+  $("#level-title").text("Level " + level);
+  h2=document.querySelector("h2");
+  h2.innerText=``;
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColor = buttonColors[randomNumber];
+  gamePattern.push(randomChosenColor);
+
+  $("#" + randomChosenColor).addClass("flash");
+  setTimeout(function() {
+    $("#" + randomChosenColor).removeClass("flash");
+  }, 200);
+  playSound(randomChosenColor);
+}
+
+// Function to check the player's answer
+function checkAnswer(currentLevel) {
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+    if (userClickedPattern.length === gamePattern.length) {
+      setTimeout(function() {
+        nextSequence();
+      }, 1000);
     }
-})
-function levelUp(){
-    userseq=[];
-    ++level;
-    max=Math.max(max,level);
+  } else {
+    playSound("wrong");
     h2=document.querySelector("h2");
-    h2.innerText=`Level ${level}`;
-    let select=btns[Math.floor(Math.random()*4)];
-    gameseq.push(`${select}`);
-    console.log(gameseq);
-    gameFlash(document.querySelector(`.${select}`));
+    h2.innerHTML=`Game Over! Current Score <b>${level}</b></br> Highest Score <b>${max}</b></br>  Press any key to start.`;
+    document.querySelector("body").style.backgroundColor="red";
+    setTimeout(()=>{document.querySelector("body").style.backgroundColor="#2c3e50"},150)
+    startOver();
+  }
 }
-function gameFlash(btn){
-    btn.classList.add("flash");
-    setTimeout(()=>{btn.classList.remove("flash")},250);
+
+// Function to restart the game
+function startOver() {
+  level = -1;
+  gamePattern = [];
+  started = false;
 }
-let allbtns=document.querySelectorAll(".btn");
-for(btn of allbtns){
-btn.addEventListener("click",btnpress)
+
+// Function to play sound based on color
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
 }
-function userFlash(btn){
-    btn.classList.add("userFlash");
-    setTimeout(()=>{btn.classList.remove("userFlash")},150);
-}
-function btnpress(){
-    userFlash(this);
-    userseq.push(this.getAttribute("id"));
-    console.log(userseq);
-    checkAns(userseq.length-1);
-}
-function checkAns(idx){
-    if(userseq[idx]==gameseq[idx]){
-        if(userseq.length==gameseq.length)
-        {
-            levelUp();
-        }
-    }
-    else{
-        h2=document.querySelector("h2");
-        h2.innerHTML=`Game Over! Current Score <b>${level}</b></br> Highest Score <b>${max}</b></br>  Press any key to start.`;
-        document.querySelector("body").style.backgroundColor="red";
-        setTimeout(()=>{document.querySelector("body").style.backgroundColor="white"},150)
-        reset();
-    }
-}
-function reset(){
-    gameseq=[];
-    userseq=[];
-    start=false;
-    level=-1;
+
+// Function to animate button press
+function animatePress(currentColor) {
+  $("#" + currentColor).addClass("userFlash");
+  setTimeout(function() {
+    $("#" + currentColor).removeClass("userFlash");
+  }, 150);
 }
